32570647770968bc2966e759264d112148dd71e7
Carlos Henrique <carlos4397@yahoo.com>
2025-02-27 11:55:05 -0300

    Create Pessoa model with name and age fields Update MessageController to return Pessoa object in JSON format Add Lombok annotations to Pessoa model (getter, setter, constructor) Use Lombok to reduce boilerplate code in Pessoa model

--- first-site-spring/src/main/java/com/example/first_site_spring/controller/MessageController.java
+++ first-site-spring/src/main/java/com/example/first_site_spring/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.example.first_site_spring.controller;
 
+import com.example.first_site_spring.model.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +16,8 @@ public class MessageController {
         return "Olá! Esta é sua primeira API REST com Spring Boot!";
     }
 
-    @GetMapping("/welcome")
-    public String welcome(@RequestParam String name) {
-        return "Olá, " + name + "! Bem vindo a sua primeira API REST!";
+    @GetMapping("/person")
+    public Person person(@RequestParam String name, @RequestParam int age) {
+        return new Person(name, age);
     }
 }
---
+++ first-site-spring/src/main/java/com/example/first_site_spring/model/Person.java
@@ -0,0 +1,13 @@
+package com.example.first_site_spring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private int age;
+}
